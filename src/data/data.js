f918b8e46ef23261390c2846c2a835774d8ffe5e
adambouj <adamkingking1@gmail.com>
2023-01-27 17:26:34 +0100

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,21 @@
+import WorkIm from '../assets/projects/work.png'
+import RealEst from '../assets/projects/realestate.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+   
+
+]
